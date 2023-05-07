7851eed2bf763b382fa7778ffd51af95a173926a
Rushaway <rushaway@hotmail.fr>
2023-05-07 19:05:02 +0200

    Bump version

--- sourcebans-web-theme-fluent/theme.conf.php
+++ sourcebans-web-theme-fluent/theme.conf.php
@@ -1,12 +1,12 @@
 <?php
 // Set the name of this theme here
-define('theme_name', "SourceBans++ Fluent Design Theme Edition");
+define('theme_name', "SourceBans++ Fluent Design Theme Edition [PHP 7.4]");
 
 // Set the author of this theme here
 define('theme_author', "aXenDev + .Rushaway");
 
 // Set the version of the theme here
-define('theme_version', "1.7.0 | Rev: 101");
+define('theme_version', "1.7.0 | Git: 164");
 
 // Set the link of the theme here
 define('theme_link', "https://github.com/Rushaway/sourcebans-web-theme-fluent/");
